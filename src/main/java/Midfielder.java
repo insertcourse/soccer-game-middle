f0e8a65c2de61331840c20dd7cee7ff618b63d75
Yujin <jin37091289@gmail.com>
2023-05-22 13:51:59 +0900

    feat : 공차기 기능

--- src/main/java/Midfielder.java
+++ src/main/java/Midfielder.java
@@ -1,9 +1,15 @@
+import java.util.Random;
+
 public class Midfielder extends Soccer{
     Midfielder(String name){
         super(name);
     }
 
     public boolean shoot(){
+        Random random = new Random();
+
+        int shoot = random.nextInt(10);
+
         return true;
     }
 }
