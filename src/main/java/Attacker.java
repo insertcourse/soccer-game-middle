cc265bc6f6e80d951c151ade5c9faf68ba231a5c
J1W0N-1209 <nabook754@naver.com>
2023-05-24 01:51:13 +0900

    feat : Attacker 클래스 구현

---
+++ src/main/java/Attacker.java
@@ -0,0 +1,19 @@
+import java.util.Random;
+
+public class Attacker extends Player{
+    Random random = new Random();
+    public Attacker(String name) {
+        super(name);
+    }
+
+    public boolean shoot(Goalkeeper goalkeeper) {
+        int shoot1 = random.nextInt(11);
+        int shoot2 = random.nextInt(11);
+
+        if(goalkeeper.block(shoot1) || goalkeeper.block(shoot2)) {
+            goal();
+            return true;
+        }
+        return false;
+    }
+}
