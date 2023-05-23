bdfda95d33202b75422d8fcac189e2a8427504c0
J1W0N-1209 <nabook754@naver.com>
2023-05-24 01:54:39 +0900

    feat : Midfielder 클래스 구현

---
+++ src/main/java/Midfielder.java
@@ -0,0 +1,18 @@
+import java.util.Random;
+
+public class Midfielder extends Player{
+    Random random = new Random();
+
+    public Midfielder(String name) {
+        super(name);
+    }
+
+    public boolean shoot(Goalkeeper goalkeeper) {
+        int shoot = random.nextInt(11); // 0 ~ 11
+        if(goalkeeper.block(shoot)) {
+            goal();
+            return true;
+        }
+        return false;
+    }
+}
