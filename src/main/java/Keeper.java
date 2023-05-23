93f5be197018310c7a391348e3744e4235c839c8
Yujin <jin37091289@gmail.com>
2023-05-23 16:55:01 +0900

    feat : 공막기 기능 구현

--- src/main/java/Keeper.java
+++ src/main/java/Keeper.java
@@ -1,9 +1,28 @@
+import java.util.Random;
+
 public class Keeper extends Soccer{
+    private int keep_count = 3;
+    private boolean check = false;
     Keeper(String name){
         super(name);
     }
 
-    public boolean shoot(){
+    public boolean shoot(Keeper keeper){
         return false;
     }
+
+    public boolean block(int shoot){
+        Random random = new Random();
+        int blockShoot = random.nextInt(5) + 3;
+        if (shoot >= 2 && shoot <= 7 && keep_count > 0){
+            keep_count -= 1;
+            return shoot != blockShoot;
+        }
+        if (shoot >= 2 && shoot <= 8){
+            return true;
+        }
+        return false;
+    }
+
+
 }
