5f61bb050d2312cc986f03e35de0ec392b24c071
Ubinquitous <ubinquitous1@gmail.com>
2023-05-25 15:55:13 +0900

    feat add VO name

---
+++ src/main/java/vo/Name.java
@@ -0,0 +1,13 @@
+package vo;
+
+public class Name {
+    private String name;
+
+    public Name(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
