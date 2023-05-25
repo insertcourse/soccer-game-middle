b93552c5346f2d1330b71e9f125c839979fe8a70
Ubinquitous <ubinquitous1@gmail.com>
2023-05-25 15:54:13 +0900

    feat SoccerGame start

--- src/main/java/SoccerGameApplication.java
+++ src/main/java/SoccerGameApplication.java
@@ -1,5 +1,14 @@
+import game.SoccerGame;
+
 public class SoccerGameApplication {
     public static void main(String[] args) {
+        SoccerGame soccerGame = new SoccerGame();
 
+        try {
+            soccerGame.init();
+            soccerGame.start();
+        } catch (Exception e){
+            e.printStackTrace();
+        }
     }
 }
