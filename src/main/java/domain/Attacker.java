2422f46bde89cb873e04c34fdd5275408af305ce
qlido <jason7530@naver.com>
2023-05-26 10:19:25 +0900

    feat(Attacker, Midfielder) 추상클래스 상속받아 공격수, 미드필더 구현

---
+++ src/main/java/domain/Attacker.java
@@ -0,0 +1,7 @@
+package domain;
+
+public class Attacker extends Player{
+    public Attacker(String name) {
+        super(name, 2);
+    }
+}
---
+++ src/main/java/domain/Midfielder.java
@@ -0,0 +1,9 @@
+package domain;
+
+public class Midfielder extends Player{
+
+    public Midfielder(String name) {
+        super(name, 1);
+    }
+
+}
